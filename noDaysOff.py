c8e34f2fcf73f88d91838279175e960a18be5214
Ross Daly <ross@myYearOfCode.com>
2018-10-10 21:55:05 -0400

    start 7x15 github style 100 day tracker.

--- noDaysOff.py
+++ noDaysOff.py
@@ -21,6 +21,7 @@ import csv
 import os
 import json
 import sys
+import math
 
 def read_prefs():
     '''
@@ -43,18 +44,19 @@ def calc_day(year,month,day):
 		total_days=total_days+months_length[i]
 		i+=1
 	total_days=total_days+day
-	return total_days	
+	return total_days
 
 
 width=1920
-height=1080
+height=896
 year=2018
 logging_string=""
 days_worked=0
 hours_worked=0
 current_streak=0
 longest_streak=0
-box_size=int((width/31.1))
+start_day=7
+box_size=int((width/15))
 fill_color="black"
 weekend_work="rgb(255,0,0)"
 weekend_rest="rgb(50,50,50)"
@@ -103,15 +105,17 @@ for month in range(1,13):
             logging_string=logging_string+(datetime_object.strftime('%m.%d.%Y')+",None,None \n")
 
         if file_found:
+            if start_day == -1:
+                start_day = calc_day(year, month, day)
             #lookup iterated entry from the csv file.
             day_data=(csvFileArray[calc_day(year,month,day)-1]) #
-            if day_data[1]!='None': # if you worked
+            if day_data[1]!='None': # you worked
                 #detect weekend
                 if datetime.datetime.weekday(datetime_object) in [5,6]: 
                     fill_color=weekend_work
                     days_worked+=1
-                # no weekend    
-                else: 
+                   
+                else: # no weekend 
                     fill_color=weekday_work
                     days_worked+=1
                 hours_worked+=float(day_data[1])
@@ -124,22 +128,31 @@ for month in range(1,13):
                 current_streak=0
 
             # Draw today's box
-            dr.rectangle(  (( (day-1)*box_size , (month-1)*box_size ) , (  ((day-1)*box_size)+box_size  , month*box_size  )) , fill=fill_color, outline = "white")
-          
-            if day_data[1]!='None':
-                #write hours worked in today's box
-                dr.text( ((day-1)*box_size,(month-1)*box_size),str(day_data[1]),(255,255,255,10),font=fnt3)
-            if current_streak>longest_streak:
-                #update strak data
-               longest_streak=current_streak 
+            # dr.rectangle(  (( (day-1)*box_size , (month-1)*box_size ) , (  ((day-1)*box_size)+box_size  , month*box_size  )) , fill=fill_color, outline = "white")
+            day_number=calc_day(year,month,day)
+            if ( day_number >= start_day and day_number < start_day+100):
+                # Draw today's box
+                week_number=math.floor((day_number-start_day)/7)
+                day_of_week=datetime.datetime.weekday(datetime_object)+1
+                dr.rectangle(  (( (week_number-1)*box_size , (day_of_week-1)*box_size ),
+                                 ( ((week_number)*box_size) , (day_of_week)*box_size  )) , fill=fill_color, outline = "white")
+                dr.text( ((week_number-1)*box_size,(day_of_week-1)*box_size),str(week_number),(255,255,255,10),font=fnt3)
+                dr.text( ((week_number-1)*box_size,(day_of_week-1.5)*box_size),str(day_number),(255,255,255,10),font=fnt3)
+                # if day_data[1]!='None':
+                    #write hours worked in today's box
+                    # dr.text( ((week_number-1)*box_size,(day_of_week-1)*box_size),str(day_data[1]),(255,255,255,10),font=fnt3)
+
+                if current_streak>longest_streak:
+                    #update strak data
+                   longest_streak=current_streak 
 if file_found:
-	#this file found loop needed so we don't write this stuff 365 times.
-    dr.text( (0,12*box_size),"DAYS WORKED: "+str(days_worked),(255,255,255),font=fnt2)
-    dr.text( (0,12*box_size+45),"HOURS WORKED: "+str(hours_worked),(255,255,255),font=fnt2)
-    dr.text( (0,12*box_size+90),"AVG HOURS/DAY: "+str(hours_worked/days_worked),(255,255,255),font=fnt2)
-    dr.text( (0,12*box_size+135),"LONGEST STREAK: "+str(longest_streak),(255,255,255),font=fnt2)
+	#this file found loop is needed so we don't write this stuff 365 times.
+    # dr.text( (0,12*box_size),"DAYS WORKED: "+str(days_worked),(255,255,255),font=fnt2)
+    # dr.text( (0,12*box_size+45),"HOURS WORKED: "+str(hours_worked),(255,255,255),font=fnt2)
+    # dr.text( (0,12*box_size+90),"AVG HOURS/DAY: "+str(hours_worked/days_worked),(255,255,255),font=fnt2)
+    # dr.text( (0,12*box_size+135),"LONGEST STREAK: "+str(longest_streak),(255,255,255),font=fnt2)
     print (os.path.dirname(sys.argv[0])+jpg_path+datetime.datetime.today().strftime('%Y-%m-%d')+".jpg")
-    im.save(os.path.dirname(sys.argv[0])+datetime.datetime.today().strftime('%Y-%m-%d')+".jpg")
+    im.save(os.path.dirname(sys.argv[0])+jpg_path+datetime.datetime.today().strftime('%Y-%m-%d')+".jpg")
     im.show()
 
 if not file_found:
